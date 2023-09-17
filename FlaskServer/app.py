0e11bb2b398b3ef51b86db0f5231b37b96b8fac8
jivankesan <117189726+jivankesan@users.noreply.github.com>
2023-09-17 09:32:55 -0400

    Update app.py

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-co = cohere.Client("w3k6SMLDH0SFtVvCwshQoomZwONOsiM5iahj3ixY")
+co = cohere.Client("APIKEY")
 
 free_spots = 0
 ser = serial.Serial("/dev/cu.usbmodem21101", 9600)
